583e54cf22a70e0479fd3333797354f134572e19
Sebastian Gonzalez <sebastian.sgm@outlook.com>
2022-11-15 11:07:58 -0800

    Create ConvertCore Controller

---
+++ app/Http/Controllers/ConvertCore.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConvertCore extends Controller
+{
+    public function callApi(){
+
+    }
+
+    public function convertCurrency($from, $to, $amount){
+        
+    }
+
+    public function convertFormatCurrency($languageFrom, $languageTo, $amount){
+        
+    }
+}
